66d2b97a1e7d83fd520390ccefa18b43a463bd34
Srividya Subramanian <srividya.ssa@gmail.com>
2022-01-29 12:00:08 +0530

    Limit Reached Responses + Clean Servod open/close

--- app.py
+++ app.py
@@ -2,6 +2,8 @@ from importlib import import_module
 import os
 import argparse
 import time
+import signal
+import sys
 
 from flask import Flask, render_template, Response
 from flask_ngrok import run_with_ngrok
@@ -11,6 +13,13 @@ if os.environ.get('CAMERA'):
     Camera = import_module('camera_' + os.environ['CAMERA']).Camera
 else:
     from camera_pi import Camera
+    
+    
+def sigint_handler(signal, frame):
+    os.system("sudo killall servod")
+    sys.exit(0)
+signal.signal(signal.SIGINT, sigint_handler)
+
     
 parser = argparse.ArgumentParser(description='Server for controlling RemRover')
 parser.add_argument('-r','--run_remote', help = 'Use ngrok with flask to create a remote tunnel.', action='store_true')
@@ -24,8 +33,15 @@ if options.run_remote:
     run_with_ngrok(app)
     
 #Setting start up servo positions - range from (50-250)
-SERVO1 = 100
-SERVO2 = 100
+SERVO1 = 90
+SERVO2 = 80
+
+os.system("sudo ./servod")
+time.sleep(0.005)
+os.system("echo 6=%s > /dev/servoblaster"%SERVO1)
+time.sleep(0.005)
+os.system("echo 5=%s > /dev/servoblaster"%SERVO2)
+time.sleep(0.005)
 
 @app.route('/')
 def index():
@@ -53,29 +69,51 @@ def handle_message(data):
     
 @socketio.on('pan_left')
 def pan_left():
-    print('PAN LEFT COMMAND FIRED.')
-    os.system("echo 6=-2 > /dev/servoblaster")
-    time.sleep(0.005)
+    global SERVO1
+    if SERVO1 == 130:
+        socketio.emit('p_l_limit', 'Pan Left Limit reached!')
+    else:
+        print('PAN LEFT COMMAND FIRED.' + str(SERVO1))
+        SERVO1 += 2
+        os.system("echo 6=+2 > /dev/servoblaster")
+        time.sleep(0.005)
 
 @socketio.on('pan_right')
 def pan_right():
-    print('PAN RIGHT COMMAND FIRED.')
-    os.system("echo 6=+2 > /dev/servoblaster")
-    time.sleep(0.005)
+    global SERVO1
+    if SERVO1 == 50:
+        socketio.emit('p_r_limit', 'Pan Right Limit reached!')
+    else:
+        print('PAN RIGHT COMMAND FIRED.' + str(SERVO1))
+        SERVO1 -= 2
+        os.system("echo 6=-2 > /dev/servoblaster")
+        time.sleep(0.005)
     
 @socketio.on('tilt_up')
 def tilt_up():
-    print('TILT UP COMMAND FIRED.')
-    os.system("echo 5=-2 > /dev/servoblaster")
-    time.sleep(0.005)
+    global SERVO2
+    if SERVO2 == 50:
+        socketio.emit('t_u_limit', 'Tilt Up Limit reached!')
+    else:
+        print('TILT UP COMMAND FIRED.' + str(SERVO2))
+        SERVO2 -= 2
+        os.system("echo 5=-2 > /dev/servoblaster")
+        time.sleep(0.005)
     
 @socketio.on('tilt_down')
 def tilt_down():
-    print('TILT DOWN COMMAND FIRED.')
-    os.system("echo 5=+2 > /dev/servoblaster")
-    time.sleep(0.005)
+    global SERVO2
+    if SERVO2 == 126:
+        socketio.emit('t_d_limit', 'Tilt Down Limit reached!')
+    else:
+        print('TILT DOWN COMMAND FIRED.' + str(SERVO2))
+        SERVO2 += 2
+        os.system("echo 5=+2 > /dev/servoblaster")
+        time.sleep(0.005)
+    
 
 if __name__ == '__main__':
     #app.run(host='0.0.0.0', threaded=True)
     debug_value = False if options.run_remote else True
     socketio.run(app, host='0.0.0.0', debug=debug_value)
+
--- static/index.js
+++ static/index.js
@@ -1,4 +1,5 @@
 var socket = io(document.domain + ':' + location.port, { reconnection:false });
+var messageButton = document.getElementById('server_message');
     
 socket.on('connect', function() {
 	socket.emit('connected', {data: 'I\'m connected!'});
@@ -20,6 +21,41 @@ document.getElementById('t_u').addEventListener("click", function(){
 document.getElementById('t_d').addEventListener("click", function(){ 
 	socket.emit('tilt_down'); 
 });
+
+
+var orignalMessage = 'Messages from server will be displayed here.'
+
+socket.on('t_u_limit', function(message) {
+	messageButton.innerText = message
+	
+	setTimeout(function() { 
+		messageButton.innerText = orignalMessage 
+	}, 2000);
+})
+
+socket.on('t_d_limit', function(message) {
+	messageButton.innerText = message
+	
+	setTimeout(function() { 
+		messageButton.innerText = orignalMessage 
+	}, 2000);
+})
+
+socket.on('p_l_limit', function(message) {
+	messageButton.innerText = message
+	
+	setTimeout(function() { 
+		messageButton.innerText = orignalMessage 
+	}, 2000);
+})
+
+socket.on('p_r_limit', function(message) {
+	messageButton.innerText = message
+	
+	setTimeout(function() { 
+		messageButton.innerText = orignalMessage 
+	}, 2000);
+})
 
 
 
